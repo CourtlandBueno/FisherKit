d2521ef36bacc2c4e5468df28169f1fe24ceed00
Courtland Bueno <courtcrit@gmail.com>
2019-06-13 12:45:17 -0700

    fix for linux

--- Sources/FisherKit/String+MD5.swift
+++ Sources/FisherKit/String+MD5.swift
@@ -49,25 +49,14 @@ extension FisherKitWrapper where Base == String {
     }
 }
 #elseif os(Linux)
-//import glibc
+import ShellOut
 extension FisherKitWrapper where Base == String {
     var md5: String {
-//        guard let data = base.data(using: .utf8) else {
-//            return base
-//        }
-//        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-//        #if swift(>=5.0)
-//        _ = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
-//            return CC_MD5(bytes.baseAddress, CC_LONG(data.count), &digest)
-//        }
-//        #else
-//        _ = data.withUnsafeBytes { bytes in
-//            return CC_MD5(bytes, CC_LONG(data.count), &digest)
-//        }
-//        #endif
-//
-//        return digest.map { String(format: "%02x", $0) }.joined()
-        return base
+        guard let output = try? shellOut(to: "echo -n '\(base)' | md5sum") else {
+            return base
+        }
+        let trimmed = output.trimmingCharacters(in: .init(charactersIn: "-")).trimmingCharacters(in: .whitespaces)
+        return trimmed
     }
 }
 #endif
